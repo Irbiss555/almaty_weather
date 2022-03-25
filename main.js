cc5f3bb7ae23ce74ab8d05da8a86bd4c25a483f4
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-03-25 18:33:23 +0600

    #1 Исправил условные обозначения в контенте

--- main.js
+++ main.js
@@ -24,9 +24,9 @@ async function getResponse() {
 function makeDailyContent(htmlElementsArray, dailyItemsArray) {
     let tabs = $('#myTab > * > *').slice(1)
     for (let i = 0; i < htmlElementsArray.length; i++) {
-        let temp = $('<p>').text(`Температура: ${dailyItemsArray[i].temp['day']}C`)
-        let humidity = $('<p>').text(`Влажность: ${dailyItemsArray[i].humidity}%`)
-        let windSpeed = $('<p>').text(`Скорость ветра: ${dailyItemsArray[i].wind_speed} м/с`)
+        let temp = $('<p>').text(`Температура: ${dailyItemsArray[i].temp['day']}, ℃`)
+        let humidity = $('<p>').text(`Влажность: ${dailyItemsArray[i].humidity}, %`)
+        let windSpeed = $('<p>').text(`Скорость ветра: ${dailyItemsArray[i].wind_speed}, м/с`)
         let desc = $('<p>').text(`Описание: ${dailyItemsArray[i].weather[0].description}`)
         let date = new Date(dailyItemsArray[i].dt * 1000)
         let localDate = new Intl.DateTimeFormat('ru-RU', {weekday: 'long', month: '2-digit', day: "2-digit"}).format(date)
@@ -36,9 +36,9 @@ function makeDailyContent(htmlElementsArray, dailyItemsArray) {
 }
 async function buildPage(){
     let response = await getResponse()
-    let currentTemp = $('<p>').text(`Температура: ${response.current.temp}C`)
-    let currentHumidity = $('<p>').text(`Влажность: ${response.current.humidity}%`)
-    let currentWindSpeed = $('<p>').text(`Скорость ветра: ${response.current.wind_speed} м/с`)
+    let currentTemp = $('<p>').text(`Температура: ${response.current.temp}, ℃`)
+    let currentHumidity = $('<p>').text(`Влажность: ${response.current.humidity}, %`)
+    let currentWindSpeed = $('<p>').text(`Скорость ветра: ${response.current.wind_speed}, м/с`)
     let currentDesc = $('<p>').text(`Описание: ${response.current.weather[0].description}`)
     $('#current').append(currentTemp, currentHumidity, currentWindSpeed, currentDesc)
     let elements = $('#myTabContent > *').slice(1)
